0677f4250a0dfb091b26b93f97a311113890e649
VladCaffeine <107715706+VladCaffeine@users.noreply.github.com>
2024-01-03 17:05:59 -0300

    att

---
+++ Exercícios/studentAverageList.py
@@ -0,0 +1,9 @@
+notes = [0.0, 0.0, 0.0, 0.0, 0.0]
+
+index = 0
+
+while (index < 5):
+    notes[index] = float(input("Qual a nota?"))
+    index += 1
+
+print(notes)
--- Exercícios/toPrimosNumbers.py
+++ Exercícios/toPrimosNumbers.py
@@ -1,25 +1,24 @@
-while (True):
+while True:
     selectedNumber = int(input("Qual numero você quer saber se é primo? "))
 
-    # 
+    #
     #     Abaixo verificamos se dentro das divisões possíveis ao dividendo,
     #     ocorre alguma com resto 0. Caso não tenha, temos então a certeza
     #     que nosso número selecionado é um número primo. Terminando então o
     #     loop, e prosseguindo.
     #
 
-    if (selectedNumber > 1):
+    if selectedNumber > 1:
         numberIsPrime = True
-        i = 2 
-        while (i < selectedNumber):
+        i = 2
+        while i < selectedNumber:
             restDivision = selectedNumber % i
-            if (restDivision == 0):
-                print("O número %d não é primo" %(selectedNumber))
+            if restDivision == 0:
+                print("O número %d não é primo" % (selectedNumber))
                 numberIsPrime = False
                 break
             i += 1
-        if (numberIsPrime):
-            print("O número %d é primo" %(selectedNumber))
-    else:
+        if numberIsPrime:
+            print("O número %d é primo" % (selectedNumber))
+      else:
         print("O número precisa ser maior que 1")
-    
